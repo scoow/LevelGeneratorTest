b689c3871e5492abfa555605907a544c47b4f0d9
scoow <85620078+scoow@users.noreply.github.com>
2023-04-10 20:04:33 +0300

    Initial

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Controls _controls;
+    private void Awake()
+    {
+        _controls = new Controls();
+    }
+    private void OnEnable()
+    {
+        _controls.
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
